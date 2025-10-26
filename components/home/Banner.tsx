7e33a1a3cce54e9c0c90dc0ed1afd3fe110092ee
Ali_Ashraf_Abir <fazilabir88@gmail.com>
2025-10-26 23:07:31 +0600

    test

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -35,7 +35,7 @@ export default function Banner() {
                 noiseAmp={1}
                 chromaticAberration={0}
                 dither={0}
-                curvature={isMobile?1:0.09}
+                curvature={isMobile?0.001:0.09}
                 tint="#5C9944"
                 mouseReact={true}
                 mouseStrength={0.5}
